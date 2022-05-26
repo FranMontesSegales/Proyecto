7a1d86ff53d230918f6b15deea0de55cbc510c5b
FranMontesSegales <95092587+FranMontesSegales@users.noreply.github.com>
2022-05-26 17:49:12 +0200

    base de datos

---
+++ Bbdd/pokedex.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-05-2022 a las 17:48:15
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pokedex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pokemon`
+--
+
+CREATE TABLE `pokemon` (
+  `Numero` int(3) NOT NULL,
+  `Nombre` varchar(15) NOT NULL,
+  `Altura` double NOT NULL,
+  `Peso` double NOT NULL,
+  `Habilidad` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pokemon`
+--
+
+INSERT INTO `pokemon` (`Numero`, `Nombre`, `Altura`, `Peso`, `Habilidad`) VALUES
+(1, 'vulvasur', 1, 2, 'Clorofila'),
+(2, 'Ivisaur', 12, 13, 'Clorofila'),
+(3, 'Venusaur ', 2, 100, 'Espesura');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
